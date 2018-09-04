38baeff44f5ade7cae4d2e6f03018ca7a5f85d12
whynotw <tedli@itri.org.tw>
2018-09-04 18:39:36 +0800

    Modify check_label.py to sync as rotation.py

--- check_label.py
+++ check_label.py
@@ -29,13 +29,6 @@ time_interval = args.time_interval
 labeled_only = args.labeled_only
 save_video = args.save_video
 
-#if len(argv) == 1:
-#  print("$1: dataset name")
-#  quit()
-#else:
-#  dataset_input = argv[1]
-#quit()
-
 dir_image = dataset_input+"/images/"
 dir_label = dataset_input+"/labels/"
 
@@ -47,13 +40,12 @@ if save_video:
   height_image, width_image = cv2.imread(image_names[0]).shape[:2]
   output = cv2.VideoWriter('output.avi',fourcc, 50.0, (width_image,height_image))
 
-def yolo_xymm(bbox_yolo,height_image,width_image):
-  bbox_yolo = bbox_yolo.strip("\n").split()
-  category      =       bbox_yolo[0]
-  x_center_bbox = float(bbox_yolo[1])
-  y_center_bbox = float(bbox_yolo[2])
-  width_bbox    = float(bbox_yolo[3])
-  height_bbox   = float(bbox_yolo[4])
+def label2coord(label,height_image,width_image):
+  category      =       label[0]
+  x_center_bbox = float(label[1])
+  y_center_bbox = float(label[2])
+  width_bbox    = float(label[3])
+  height_bbox   = float(label[4])
   x_left   = int( (x_center_bbox- width_bbox/2.) * width_image )
   x_right  = int( (x_center_bbox+ width_bbox/2.) * width_image )
   y_top    = int( (y_center_bbox-height_bbox/2.) * height_image )
@@ -72,8 +64,9 @@ for image_name in image_names:
     image = cv2.imread(image_name)
     height_image, width_image = image.shape[:2]
 
-  for bbox_yolo in labels:
-    category, x_left, y_top, x_right, y_bottom = yolo_xymm(bbox_yolo,height_image,width_image)
+  for label in labels:
+    label = label.strip("\n").split()
+    category, x_left, y_top, x_right, y_bottom = label2coord(label,height_image,width_image)
     cv2.rectangle(image,(x_left,y_top),(x_right,y_bottom),(255,255,255),1)
 
   cv2.imshow("test",image)
